d50d4228f2ec0efe00ec761c2a02b04ad530fcd7
AmishaS31 <138499105+AmishaS31@users.noreply.github.com>
2024-01-04 12:55:43 +0530

    commit

--- to-do-list/src/components/Assets/CSS/Todo.jsx
+++ to-do-list/src/components/Assets/CSS/Todo.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react';
 import '../CSS/Todo.css'
+import Todoitems from './Todoitems';
 
 const Todo = () => {
+
+const [todos,setTodos]=useState([]);
+const inputRef = useRef(null);
+
+
+let count =0;
+const add = () =>{
+   
+  setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}])
+  inputRef.current.value="";
+}
+
+useEffect(()=>{
+    console.log(todos);
+},[todos])
+
+
   return (
     <div className='todo'>
         <div className="todo-header">To-do List</div>
         <div className='todo-add'>
-            <input type="text" placeholder='Add your Task' className='todo-input'></input>
-            <div className="todo-add-btn">ADD</div>
+            <input ref={inputRef} type="text" placeholder='Add your Task' className='todo-input'></input>
+            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list"></div>
+        {todos.map((item,index)=>{
+            return <Todoitems key={index} no={item.no} display={item.display} text={item.text}/>
+        })
+      }
     </div>
   )
 }
--- to-do-list/src/components/Assets/CSS/Todoitems.jsx
+++ to-do-list/src/components/Assets/CSS/Todoitems.jsx
@@ -1,9 +1,17 @@
-
 import './CSS/Todoitems.css'
+import tick from './components/Assets/tick.png'
+import not_tick from './components/Assets/not_tick.png'
+import cross from './components/Assets/cross.png'
+
 const Todoitems = () => {
   return (
-    <div>
-      
+    <div className='todoitems'>
+      <div className='todoitems-container'>
+      <img src={not_tick} alt=""/>
+      <img src={tick} alt=""/>
+      <div className='todoitems-text'></div>
+      </div>
+      <img src={cross} alt=""/>
     </div>
   )
 }
